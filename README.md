811008dc2b546ec6928b867997b55394315b3ed0
Caosticc <gajose2009@protonmail.com>
2023-05-08 17:04:24 -0300

    Banner function and some comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Morse_Code_Conversor
 
 Simple Usage:
-Press 1 t0 convert text to Morse 
-Press 2 to convert Morseto text
+Press 1 to convert text to Morse 
+Press 2 to convert Morse to text
--- conversor codigo morse.py
+++ conversor codigo morse.py
@@ -2,18 +2,29 @@ import cli_box
 import pyfiglet
 from colorama import Fore
 
-ascii_art = pyfiglet.Figlet()
-info =  f"\n\n{Fore.MAGENTA}"\
-f"{Fore.RESET}{'-'*20}"
+def banner():
+    ascii_art = pyfiglet.Figlet()
+    info =  f"\n\n{Fore.MAGENTA}"\
+    f"{Fore.RESET}{'-'*20}"
+    
+    author = "Author: Caostic\nDate: 04/05/2023\nVersion: v1.0"
+    print(Fore.MAGENTA + ascii_art.renderText("Morse Code Conversor") + Fore.RESET + author + info)
+    print(cli_box.box(f"{'Choose':^}\n{'[1] - For text to morse ':^} \n{'[2] - For morse to text':^} ",corners=('+','+','+','+'), sides=('|','-')))
 
-author = "Author: Caostic\nDate: 04/05/2023\nVersion: v1.0"
+# funcition to convert plain text into morse code
+def text_conversor(plane_text):
+    for char in plane_text:
+        text_converted = dictionary.get(char)
+        print(text_converted, end='/')
 
-print(Fore.MAGENTA + ascii_art.renderText("Morse Code Conversor") + Fore.RESET + author + info)
-print(cli_box.box(f"{'Choose':^}\n{'[1] - For text to morse ':^} \n{'[2] - For morse to text':^} ",corners=('+','+','+','+'), sides=('|','-')))
+# funcition to convert plain text into morse code
+def morse_conversor(morse_text):
+    inverse = {v: k for k, v in dictionary.items()}
+    for char in morse_text:
+        morse_converted= inverse.get(char)
+        print(morse_converted, end='')
 
-
-
-
+# dictionary with 
 dictionary = {'{':'{','}': '}',' ': ' ', 'A':'.-','B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.','G':'--.','H':'....',
 'I':'..','J':'.---','K':'-.-','L':'.-..','M':'--','N':'-.','O':'---','P':'.--.','Q':'--.-','R':'.-.',
 'S':'...','T':'-','U':'..-','V':'...-','W':'.--','X':'-..-','Y':'-.--','Z':'--..','.':'.-.-.-',',':'--..--',
@@ -21,21 +32,8 @@ dictionary = {'{':'{','}': '}',' ': ' ', 'A':'.-','B':'-...','C':'-.-.','D':'-..
 '=':'-...-','-':'-....-','_': '..--.-','"':'.-...','$':'...-..-','@':'.--.-.',
 '1':'.----','2':'..---','3':'...--','4':'....-','5':'.....','6':'-....','7':'--...','8':'---..','9':'----.', '0':'-----'}
 
-
-def text_conversor(plane_text):
-    for char in plane_text:
-        text_converted = dictionary.get(char)
-        print(text_converted, end='/')
-
-def morse_conversor(morse_text):
-    inverse = {v: k for k, v in dictionary.items()}
-    for char in morse_text:
-        morse_converted= inverse.get(char)
-        print(morse_converted, end='')
-
-
-
 try:
+    banner()
     option=int(input("\nChoose the option: "))
     if option not in [1,2]:
         raise ValueError("Invalid option. Choose 1 or 2.") 
@@ -51,3 +49,6 @@ try:
 
 except ValueError as error:
     print(f"Error: {error}")
+
+
+
