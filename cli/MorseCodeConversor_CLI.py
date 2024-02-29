8d2b65619ba8561da4ff1aa46516d3a2fc1eda17
Caosticc <109768693+Caosticc@users.noreply.github.com>
2024-02-29 11:48:42 -0300

    Update
    The version of the cli file was improved, some changes were made and the cli folder was added to the repository

---
+++ cli/MorseCodeConversor_CLI.py
@@ -0,0 +1,61 @@
+import cli_box
+import pyfiglet
+from colorama import Fore, init
+
+init()
+
+def banner():
+    ascii_art = pyfiglet.Figlet()
+    info = f"\n\n{Fore.MAGENTA}"\
+           f"{Fore.RESET}{'-'*20}"
+
+    author = "Author: Caostic\nDate: 04/05/2023\nVersion: v1.1"
+    print(Fore.MAGENTA + ascii_art.renderText("Morse Code Conversor") + Fore.RESET + author + info)
+    print(cli_box.box(f"{'Choose':^}\n{'[1] - For text to morse ':^} \n{'[2] - For morse to text':^} ", corners=('+', '+', '+', '+'), sides=('|', '-')))
+    print(Fore.RESET)
+
+# function to convert plain text into morse code
+def text_conversor(plane_text):
+    for char in plane_text:
+        text_converted = dictionary.get(char)
+        print(text_converted, end='/')
+
+# function to convert plain text into morse code
+def morse_conversor(morse_text):
+    inverse = {v: k for k, v in dictionary.items()}
+    for word in morse_text:
+        for char in word.split(' '):
+            morse_converted = inverse.get(char)
+            print(morse_converted, end='')
+        print(' ', end='')
+
+
+# main code
+def main():
+    try:
+        banner()
+        option = int(input("\nChoose the option: "))
+        if option not in [1, 2]:
+            raise ValueError("Invalid option. Choose 1 or 2.")
+        if option == 1:
+            plane_text = input("\nPut your text to turn into morse: ").upper()
+            text_conversor(plane_text)
+        if option == 2:
+            morse_text = input("Put your morse to turn into text: ").split()
+            morse_conversor(morse_text)
+
+    except ValueError as error:
+        print(f"Error: {error}")
+
+# dictionary with letters and their respective morsecode symbols
+dictionary = {'{': '{', '}': '}', ' ': ' ', 'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+              'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+              'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--',
+              'X': '-..-', 'Y': '-.--', 'Z': '--..', '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.',
+              '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.',
+              '=': '-...-', '-': '-....-', '_': '..--.-', '"': '.-...', '$': '...-..-', '@': '.--.-.', '1': '.----',
+              '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+              '9': '----.', '0': '-----'}
+
+if __name__ == '__main__':
+    main()
